d6482dde0a850484585249a4f9cafd1d53b9960c
Brie Flynn <65606659+briannaflynn@users.noreply.github.com>
2021-02-12 08:25:01 -0600

    added comment about memory, dimensions

--- jsonIterator.py
+++ jsonIterator.py
@@ -17,4 +17,6 @@ if __name__ == "__main__":
 	
 	json_Iterator(JSON_path, image_path)
 	
-	
+'''
+Double and triple check the dimensions of the mask files! issues with memory can cause the masks to be offset in height and will cause problems during training (always check training data as well to make sure there's no dimension matching issues)
+'''
