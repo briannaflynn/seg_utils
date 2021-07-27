25c1bbb7847f2031a8d62fcd7ce937b3e4cbeb87
Brie Flynn <brianna.flynn@utexas.edu>
2021-07-26 21:47:17 -0500

    cleaned up, option to output a dataframe now included

--- interp-metrics.py
+++ interp-metrics.py
@@ -10,40 +10,18 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser(description="Interpret predictions using pixel accuracy, mean IOU, and per polygon IOU")
-# parser.add_argument('--y_true', action='store', type=str, required = True, help ='Path to ground truth image')
-# parser.add_argument('--y_pred', action='store', type=str, required = True, help='Path to prediction image')
 parser.add_argument('--polygon', action='store', type=str, default=None, help= 'Provide polygon list for ious, current option is: spine')
 parser.add_argument('--dir_y_true', action = 'store', type = str, required = True, help = 'Directory with ground truth images')
 parser.add_argument('--dir_y_pred', action = 'store', type = str, required = True, help = 'Directory with predictions') 
 parser.add_argument('--df_name', action = 'store', type = str, default = None, help = 'Create a dataframe with results')
 
 args = parser.parse_args()
-# y_true_name = args.y_true
-# y_pred_name = args.y_pred
+
 poly = args.polygon
 true_dir = args.dir_y_true
 pred_dir = args.dir_y_pred
 df_name = args.df_name
 
-# y_true = imread(y_true_name)
-# y_pred = imread(y_pred_name)
-# 
-# p_a = pixel_accuracy(y_pred, y_true)
-# m_i = mean_IU(y_pred, y_true)
-# 
-# mean_iou = m_i[0]
-# polygon_ious = m_i[1]
-# 
-# if poly == "spine":
-# 	polygons = ["T12", "L1", "L2", "L3", "L4", "L5"]
-# 	polydict = dict(zip(polygons, polygon_ious))
-# 	metrics = {"Filename": y_pred_name, "Pixel Accuracy": p_a, "Mean IOU": mean_iou, "Individual Polygon IOUs": polydict}
-# 	print(metrics)
-# 
-# elif poly == None:
-# 	metrics = {"Filename": y_pred_name, "Pixel Accuracy": p_a, "Mean IOU": mean_iou, "Individual Polygon IOUs": polygon_ious}
-# 	print(metrics)
-	
 metric_list = []
 
 if true_dir and pred_dir != None:
