44d58148b01eb4935cbdc9c67ecdd28b5fba8d20
Brie Flynn <brianna.flynn@utexas.edu>
2021-07-28 12:23:45 -0500

    make dicom pad and annotation pad independent of one another

--- padder.py
+++ padder.py
@@ -57,6 +57,8 @@ def annotation_to_padded_image(annotation_dir, row_val:tuple, col_val:tuple, hei
 	def ext_only(directory, extension='png'):
 		return([f for f in os.listdir(directory) if f.endswith('.' + extension)])
 	
+	z = 0
+	w = 0
 	for file in ext_only(annotation_dir):
 		img = os.path.join(annotation_dir, file)
 		image = Image.open(os.path.join(annotation_dir, file))
@@ -74,18 +76,22 @@ def annotation_to_padded_image(annotation_dir, row_val:tuple, col_val:tuple, hei
 			outimg = outimg.replace('.dcm', padded_string)
 			cv2.imwrite(outimg, padded_image)
 			print(outimg, "written!")
+			z += 1
 		else:
 			print('size requirement not met')
+			w += 1
 		
+		print("\nFile padding complete -")
+		print(z, "files were written and", w, "files did not meet the size requirement set by the user")
 		return None
 
 if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser(description="Convert images to padded images")
 	
-	parser.add_argument('--dicom_directory', action='store', type=str, required = True, help ='Path to DICOM image directory')
+	parser.add_argument('--dicom_directory', action='store', type=str, default = None, help ='Path to DICOM image directory')
 	parser.add_argument('--annotation_directory', action='store', type=str, default = None, help ='Path to annotation image directory')
-	parser.add_argument('--jpg_output_directory', action ='store', type = str, required = True, help = 'Path to output directory for padded JPG images')
+	parser.add_argument('--jpg_output_directory', action ='store', type = str, required = None, help = 'Path to output directory for padded JPG images')
 	parser.add_argument('--annotation_output_directory', action = 'store', type = str, default = None, help = 'Path to padded annotation directory')
 	parser.add_argument('--row_values', action = 'store', nargs = '+', required = True, help = 'Tuple containing range for row value')
 	parser.add_argument('--column_values', action = 'store', nargs = '+', required = True, help = 'Tuple containing range for column values')
@@ -103,7 +109,10 @@ if __name__ == "__main__":
 	height = args.padded_height
 	width = args.padded_width
 		
-	dicom_to_padded_image(dcm, rows, cols, height, width, jpg_out, img_type = ".jpg")
+	
+	if dcm and jpg_out != None:
+		dicom_to_padded_image(dcm, rows, cols, height, width, jpg_out, img_type = ".jpg")
+	
 	
 	if annots and annots_out != None:
 		annotation_to_padded_image(annots, rows, cols, height, width, annots_out)
